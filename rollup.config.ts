97cf4a72f962619781177ffad301aa11d8fbbce4
Seanghay Yath <seanghay.dev@gmail.com>
2022-07-24 13:52:49 +0700

    chore: add cjs

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,21 +1,25 @@
 import { defineConfig } from "rollup";
 import run from "@rollup/plugin-run";
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import esbuild from 'rollup-plugin-esbuild'
+import resolve from "@rollup/plugin-node-resolve";
+import esbuild from "rollup-plugin-esbuild";
+import pkg from "./package.json";
 
 const dev = process.env.NODE_ENV !== "production";
 
 export default defineConfig({
   input: "src/main.ts",
-  output: {
-    file: "dist/main.js",
-    format: "esm",
-  },
+  output: [
+    { file: pkg.main, format: "cjs" },
+    { file: pkg.module, format: "esm" }
+  ],
   plugins: [
     esbuild({}),
-    nodeResolve({}),
-    dev && run({
-      args: ["-r", "dotenv/config"],
+    resolve({
+      extensions: [".ts", ".js"],
     }),
+    dev &&
+      run({
+        args: ["-r", "dotenv/config"],
+      }),
   ],
 });
